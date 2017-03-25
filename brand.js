8340195ebe1f5b60c64e146ea11309a7286d44be
Eric Wong <erwong@stanfordhealthcare.org>
2017-03-25 01:51:32 -0700

    Added Brands and brand carousel

---
+++ brand.js
@@ -0,0 +1,102 @@
+if(!window.sunhing){window.sunhing = {}}
+window.sunhing.brand = [
+
+//--------------------- Start of Brand ---------------------//
+{
+title: 'Longevity Brand',
+subtitle: 'Variety of Products',
+image: 'images/brand/logolong.jpg'
+},
+{
+title: 'Cafe Longevity',
+subtitle: 'Ground Coffee',
+image: 'images/brand/logocafel.jpg'
+},
+{
+title: 'Black & White',
+subtitle: 'Dairy Products',
+image: 'images/brand/logobw.jpg'
+},
+{
+title: 'Parrot',
+subtitle: 'Variety of Products',
+image: 'images/brand/logopar.gif'
+},
+{
+title: 'Cow & Mill',
+subtitle: 'Dairy Products',
+image: 'images/brand/logocm.jpg'
+},
+{
+title: 'Dairy Girl',
+subtitle: 'Dairy Products',
+image: 'images/brand/logodg.gif'
+},
+{
+title: 'Hoa Lan',
+subtitle: 'Dairy Products',
+image: 'images/brand/logohl.jpg'
+},
+{
+title: 'Flower Band',
+subtitle: 'Variety of Products',
+image: 'images/brand/logoflwr.gif'
+},
+{
+title: 'Fortune',
+subtitle: 'Variety of Products',
+image: 'images/brand/logofor.gif'
+},
+{
+title: 'Falcon',
+subtitle: 'Variety of Products',
+image: 'images/brand/logofal.gif'
+},
+{
+title: 'Sun Hing',
+subtitle: 'Luncheon Meat',
+image: 'images/brand/logoshf.gif'
+},
+{
+title: 'Fuyuki Brand',
+subtitle: 'Dried Mushrooms',
+image: 'images/brand/logofuy.gif'
+},
+{
+title: 'Beauty',
+subtitle: 'Variety of Products',
+image: 'images/brand/logobea.gif'
+},
+{
+title: 'Reina',
+subtitle: 'Rice Products',
+image: 'images/brand/logore.gif'
+},
+{
+title: 'Medell',
+subtitle: 'Luncheon Meat',
+image: 'images/brand/logomed.gif'
+},
+{
+title: 'Grand Palace',
+subtitle: 'Rice',
+image: 'images/brand/logogp.gif'
+},
+{
+title: 'Lee',
+subtitle: 'Biscuits',
+image: 'images/brand/logolee.gif'
+},
+{
+title: 'Dr. Oetker',
+subtitle: 'Dessert Products',
+image: 'images/brand/logodro.gif'
+},
+{
+title: 'Famous House',
+subtitle: 'Dessert Drinks',
+image: 'images/brand/logofh.gif'
+},
+//--------------------- End of Brand ---------------------//
+
+];
--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-$(function(){});
+$(function(){
+    if(!window.sunhing){window.sunhing = {}}
+    var checkPage = function(){
+        createParams(window.location.hash);
+    };
+    var createParams = function(hash){
+        if(!window.sunhing.params){
+            window.sunhing.params = {};
+        }
+        hash = hash.replace(/^#/,'');
+        var hashArray = hash.split('&');
+        if(hashArray && hashArray.length){
+            $.each(hashArray, function(i, paramString){
+                var keyValue = paramString.split('=');
+                if(keyValue && keyValue.length === 2){
+                    window.sunhing.params[keyValue[0]] = keyValue[1];
+                }
+            });
+        }
+        if(!window.sunhing.params.page){
+            window.sunhing.params.page = 'home';
+        }
+        setPage(window.sunhing.params.page);
+    };
+    var setPage = function(page){
+        if(window.sunhing.currentPage !== page){
+            window.sunhing.currentPage = page;
+            $('.data-div').empty();
+            $('.sidenav .list-group-item').removeClass('active');
+            $('.sidenav .list-group-item.' + page).addClass('active');
+            $('.pages').hide();
+            $('.page-' + page).show();
+            switch(page){
+                case 'home':
+                    listBrand($('.brand-carousel'));
+                    makeCarousel($('.brand-carousel'));
+                    break;
+            }
+        }
+    };
+    var listBrand = function(page){
+        if(window.sunhing.brand && window.sunhing.brand.length){
+            $.each(window.sunhing.brand, function(i, brand){
+                page.append('<div class="brand-item item"><h2>' + brand.title + '</h2><img src="' + brand.image + '" /><h3>' + brand.subtitle + '</h3></div>');
+            });
+        }
+    };
+    var makeCarousel = function(page){
+        var items = page.find('.item'),
+            duration = 3000,
+            effect = 1000;
+        var nextItem = function(){
+            var current = page.find('.item.active');
+            if(current && current.length){
+                current.removeClass('active').fadeOut(effect, function(){
+                    var next = current.next('.item');
+                    if(!next || !next.length){
+                        next = $(items[0]);
+                    }
+                    next.addClass('active').fadeIn(effect, function(){
+                        setTimeout(nextItem, duration);
+                    });
+                });
+            }
+        };
+        items.hide().removeClass('active');
+        $(items[0]).addClass('active').fadeIn(effect, function(){
+            setTimeout(nextItem, duration);
+        });
+    };
+    $(window).on('hashchange', function(e){
+        checkPage();
+    });
+    window.sunhing.currentPage = '';
+    checkPage();
+});
