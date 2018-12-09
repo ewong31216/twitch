67ec85424f8d8775b7d16e1a8a8678ddfebaf868
Eric Wong <erwong@stanfordhealthcare.org>
2018-12-09 13:35:51 -0800

    Kubit:build

---
+++ kubit/build/precache-manifest.b0676a24b1d111e2f3832318e293360f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7d7baeef6da75dc6994",
+    "url": "/static/js/main.a7d7baee.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "a7d7baeef6da75dc6994",
+    "url": "/static/css/main.66cab20e.chunk.css"
+  },
+  {
+    "revision": "ef545409c505ccba9ea08b22eb1bf572",
+    "url": "/index.html"
+  }
+];
